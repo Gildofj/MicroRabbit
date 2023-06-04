a6a50802cfb9e423bf81243fe489c42cb0892641
Gildo Junior <1gildojunior@gmail.com>
2023-06-03 23:04:09 -0300

    Initial commit with EventBus interfaces

---
+++ MicroRabit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿
+using MicroRabit.Domain.Core.Commands;
+using MicroRabit.Domain.Core.Events;
+
+namespace MicroRabit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ MicroRabit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabit.Domain.Core.Events;
+
+namespace MicroRabit.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+
+    }
+}
