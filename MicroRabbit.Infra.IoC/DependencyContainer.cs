0bdabde7070df0d41863818fa46aa33be9c77fb9
Gildo Junior <1gildojunior@gmail.com>
2023-06-03 23:39:12 -0300

    implement IoC project

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Infra.Bus;
+using MicroRabit.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
